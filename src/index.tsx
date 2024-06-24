93b9e589976c37bb2aca8fe971494d572aeee9de
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-06-24 16:55:08 -0300

    feat: adicionando links do menu

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Router from './routes';
+import Menu from 'components/Menu';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Menu />
     <Router />
   </React.StrictMode>
 );
--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,4 +1,3 @@
-import Menu from 'components/Menu';
 import styles from './Inicio.module.scss';
 import { itens } from "pages/Cardapio/Itens";
 
@@ -7,7 +6,6 @@ export default function Inicio() {
     pratosRecomendados = pratosRecomendados.sort(() => 0.5 - Math.random()).splice(0, 3);
     return (
         <section>
-            <Menu />
             <h3 className={styles.titulo}>
                 Recomendações da cozinha
             </h3>
