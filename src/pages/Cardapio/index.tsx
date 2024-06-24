e6286361699dbf1844c9539cd078c194114c03c6
Vitor <vitormarinheiro9@gmail.com>
2024-06-23 22:58:15 -0300

    style: adicionando estilo para o componente Cardapio

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,5 +1,6 @@
 import styles from './Cardapio.module.scss';
 import {ReactComponent as Logo} from 'assets/logo.svg'
+import 'normalize.css'
 
 export default function Cardapio() {
   return (
@@ -7,6 +8,11 @@ export default function Cardapio() {
         <nav className={styles.menu}>
           <Logo />
         </nav>
+        <header className={styles.header}>
+          <div className={styles.header__text}>
+            A casa do código e da massa
+          </div>
+        </header>
     </main>
   )  
 }
