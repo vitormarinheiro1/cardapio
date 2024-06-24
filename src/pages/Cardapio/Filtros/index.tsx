df143c4128a1eec86ead02dcbb9f3bf59d9b62c8
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-06-24 08:52:44 -0300

    feat: adicionando Filtros

--- src/pages/Cardapio/Filtros/index.tsx
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -1,7 +1,50 @@
 import styles from './Filtros.module.scss'
+import classNames from "classnames";
 
-export default function Filtros() {
+type IOpcao = typeof filtros[0];
+
+interface Props {
+    filtro: number | null;
+    setFiltro: React.Dispatch<React.SetStateAction<number | null>>
+}
+
+const filtros = [
+    {
+        "label": "Massas",
+        "id": 1
+    },
+    {
+        "label": "Carnes",
+        "id": 2
+    },
+    {
+        "label": "Combos",
+        "id": 3
+    },
+    {
+        "label": "Veganos",
+        "id": 4
+    }
+]
+
+export default function Filtros({ filtro, setFiltro }: Props) {
+    function selecionarFiltro(opcao: IOpcao) {
+        if (filtro === opcao.id) return setFiltro(null);
+        return setFiltro(opcao.id);
+    }
     return (
-        <div></div>
+        <div className={styles.filtros}>
+            {filtros.map((opcao) => (
+                <button
+                    className={classNames({
+                        [styles.filtros__filtro]: true,
+                        [styles['filteros__filtro--ativo']]: filtro === opcao.id
+                    })}
+                    key={opcao.id} onClick={() => selecionarFiltro(opcao)}
+                >
+                    {opcao.label}
+                </button>
+            ))}
+        </div>
     )
 }
