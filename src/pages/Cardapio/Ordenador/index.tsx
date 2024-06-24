e679a7b5849c229f3568e4b4bd38babe6a83b396
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-06-24 09:40:08 -0300

    feat: adicionando ordenador

---
+++ src/pages/Cardapio/Ordenador/index.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import styles from './Ordenador.module.scss';
+import classNames from 'classnames';
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from 'react-icons/md'
+
+interface Props {
+    ordenador: string,
+    setOrdenador: React.Dispatch<React.SetStateAction<string>>
+}
+
+const opcoes = [
+    {
+        "nome": "",
+        value: ""
+    },
+    {
+        "nome": "Porção",
+        value: "porcao"
+    },
+    {
+        "nome": "Qtd. pessoas",
+        value: "qtd_pessoas"
+    },
+    {
+        "nome": "Preço",
+        value: "preco"
+    },
+]
+
+export default function Ordenador({ ordenador, setOrdenador}: Props) {
+    const [aberto, setAberto] = useState(false);
+    const nomeOrdenador = ordenador && opcoes.find(opcao => opcao.value === ordenador)?.nome;
+    return (
+        <button
+            className={classNames({
+                [styles.ordenador]: true,
+                [styles["ordenador--ativo"]]: ordenador !== "",
+            })}
+            onClick={() => setAberto(!aberto)}
+            onBlur={() => setAberto(false)}
+        >
+            <span>{nomeOrdenador || "Ordenar Por"}</span>
+            {aberto ? <MdKeyboardArrowUp size={20} /> : <MdKeyboardArrowDown size={20} />}
+            <div className={classNames({
+                [styles.ordenador__options]: true,
+                [styles["ordenador__options--ativo"]]: aberto,
+            })}>
+                {opcoes.map(opcao => (
+                    <div className={styles.ordenador__option} key={opcao.value} onClick={() => setOrdenador(opcao.value)}>
+                        {opcao.nome}
+                    </div>
+                ))}
+            </div>
+        </button>
+    )
+}
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import styles from './Cardapio.module.scss';
-import {ReactComponent as Logo} from 'assets/logo.svg'
-import 'normalize.css'
+import {ReactComponent as Logo} from 'assets/logo.svg';
+import 'normalize.css';
 import Buscador from './Buscador';
 import Filtros from './Filtros';
+import Ordenador from './Ordenador';
 
 export default function Cardapio() {
   const [busca, setBusca] = useState("");
   const [filtro, setFiltro] = useState<number | null>(null);
+  const [ordenador, setOrdenador] = useState("");
   return (
     <main>
         <nav className={styles.menu}>
@@ -23,6 +25,7 @@ export default function Cardapio() {
           <Buscador busca={busca} setBusca={setBusca} />
           <div className={styles.cardapio__filtros}>
             <Filtros filtro={filtro} setFiltro={setFiltro} />
+            <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
           </div>
         </section>
     </main>
