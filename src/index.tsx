a0077dc214e446c2ff1f32059cc592fda66d2054
Vitor <vitormarinheiro9@gmail.com>
2024-06-24 21:49:04 -0300

    fix: corrigindo erro do menu em routes.js

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Router from './routes';
-import Menu from 'components/Menu';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Menu />
     <Router />
   </React.StrictMode>
 );
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
+import Menu from 'components/Menu';
 
 export default function AppRouter() {
     return (
         <Router>
+            <Menu />
             <Routes>
                 <Route path='/' element={<Inicio />} />
                 <Route path='/cardapio' element={<Cardapio />} />
