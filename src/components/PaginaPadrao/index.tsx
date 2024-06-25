e90b35c72544b5463e8e4434c1d0e841388625e0
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-06-25 17:38:13 -0300

    fix: corrigindo bug do header

--- src/components/PaginaPadrao/index.tsx
+++ src/components/PaginaPadrao/index.tsx
@@ -2,7 +2,7 @@ import styles from './PaginaPadrao.module.scss';
 import stylesTema from "styles/Tema.module.scss";
 import { Outlet } from 'react-router-dom';
 
-export default function PaginaPadrao() {
+export default function PaginaPadrao({ children }: { children?: React.ReactNode}) {
     return (
         <>
             <header className={styles.header}>
@@ -12,6 +12,7 @@ export default function PaginaPadrao() {
             </header>
             <div className={stylesTema.container}>
                 <Outlet />
+                {children}
             </div>
         </>
     );
