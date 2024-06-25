731889ce9787b44ac00a20302bf4e23c192c75c6
Vitor <vitormarinheiro9@gmail.com>
2024-06-24 22:53:37 -0300

    feat: terminando a tela inicio e adicionando page sobre

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styles from './Cardapio.module.scss';
+import stylesTema from "styles/Tema.module.scss";
 import 'normalize.css';
 import Buscador from './Buscador';
 import Filtros from './Filtros';
@@ -11,21 +12,14 @@ export default function Cardapio() {
   const [filtro, setFiltro] = useState<number | null>(null);
   const [ordenador, setOrdenador] = useState("");
   return (
-    <main>
-      <header className={styles.header}>
-        <div className={styles.header__text}>
-          A casa do código e da massa
-        </div>
-      </header>
-      <section className={styles.cardapio}>
-        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
-        <Buscador busca={busca} setBusca={setBusca} />
-        <div className={styles.cardapio__filtros}>
-          <Filtros filtro={filtro} setFiltro={setFiltro} />
-          <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
-        </div>
-        <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
-      </section>
-    </main>
+    <section className={styles.cardapio}>
+      <h3 className={stylesTema.titulo}>Cardápio</h3>
+      <Buscador busca={busca} setBusca={setBusca} />
+      <div className={styles.cardapio__filtros}>
+        <Filtros filtro={filtro} setFiltro={setFiltro} />
+        <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+      </div>
+      <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
+    </section>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import Menu from 'components/Menu';
+import PaginaPadrao from 'components/PaginaPadrao';
+import Sobre from 'pages/Sobre';
 
 export default function AppRouter() {
     return (
-        <Router>
-            <Menu />
-            <Routes>
-                <Route path='/' element={<Inicio />} />
-                <Route path='/cardapio' element={<Cardapio />} />
-            </Routes>
-        </Router>
+        <main>
+            <Router>
+                <Menu />
+                <Routes>
+                    <Route path='/' element={<PaginaPadrao />}>
+                        <Route index element={<Inicio />} />
+                        <Route path='cardapio' element={<Cardapio />} />
+                        <Route path='sobre' element={<Sobre/>} />
+                    </Route>
+                </Routes>
+            </Router>
+        </main>
     );
 }
